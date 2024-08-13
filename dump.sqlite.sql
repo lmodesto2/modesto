41a88ad69d58b5b7d2b63c0055de6d6a2470269a
lmodesto2 <159173738+lmodesto2@users.noreply.github.com>
2024-08-13 13:46:14 -0300

    Código SQL do dia 13/08

---
+++ dump.sqlite.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE alunos(
+  id int (3) unique,
+  nome varchar(30)not null,
+  idade varchar (3),
+  turma varchar (4)
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
