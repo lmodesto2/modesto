0d4b139dab96b697d6db20e704373b8a12317684
lmodesto2 <159173738+lmodesto2@users.noreply.github.com>
2024-08-13 15:55:33 -0300

    tabela funcionários e reservas

---
+++ funcionarios.sqlite.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE funcionarios(
+  	id int(3)UNIQUE,
+	nome varchar(30) NOT NULL,
+  	cargo varchar(15),
+  	salarios varchar (10)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
---
+++ reservar.sqlite.sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE reservas(
+  
+ 	id int(3) UNIQUE,
+  	nome_cliente varchar(30) not NULL,
+  	quarto varchar(3) not NULL,
+  	data_entrada date,
+  	data_saida date
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
